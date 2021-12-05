470273ca84dc0a6a9d1032b7d74f0a518505c6c6
Dhruv128 <84789181+Dhruv128@users.noreply.github.com>
2021-12-05 21:49:39 +0530

    Add files via upload

---
+++ chess_room.js
@@ -0,0 +1,15 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyDKhL19XcE_-G1g8vap0wUlBE9eMFuoXeQ",
+    authDomain: "chess-knights.firebaseapp.com",
+    databaseURL: "https://chess-knights-default-rtdb.firebaseio.com",
+    projectId: "chess-knights",
+    storageBucket: "chess-knights.appspot.com",
+    messagingSenderId: "508329143828",
+    appId: "1:508329143828:web:b8cfd8d7381d897e644101"
+  };
+  
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+  
+user_name=localStorage.getItem("user_name");
+document.getElementById("user_name").innerHTML="Welcome To Chess Knights"+user_name;
